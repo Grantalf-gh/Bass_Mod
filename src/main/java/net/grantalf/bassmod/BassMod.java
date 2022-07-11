fc5dcea147c53393ed9f223bcc59de58a990099e
grant <grant@thetrotter.net>
2022-07-11 14:06:30 -0800

    Initial commit

---
+++ src/main/java/net/grantalf/bassmod/BassMod.java
@@ -0,0 +1,15 @@
+package net.grantalf.bassmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BassMod implements ModInitializer {
+	public static final String MODID = "bassmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/grantalf/bassmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.grantalf.bassmod.mixin;
+
+import net.grantalf.bassmod.BassMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		BassMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
